a840ab949c55dd2d0cd73f5b784fc5a383d2ce4f
Jair Almaraz <jairalmaraz@MacBook-Pro-de-Jair.local>
2019-05-22 19:30:48 -0500

    changes to test graphql queries and mutations with graphiQL

--- app.js
+++ app.js
@@ -18,14 +18,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// graphQL
+app.use('*', cors());
+app.use(
+  '/',
+  cors(),
+  graphqlHTTP({
+    schema: schema,
+    rootValue: global,
+    graphiql: true
+  })
+);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,17 +60,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// graphQL
-app.use('*', cors());
-app.use(
-  '/graphql',
-  cors(),
-  graphqlHTTP({
-    schema: schema,
-    rootValue: global,
-    graphiql: true
-  })
-);
 
 module.exports = app;
